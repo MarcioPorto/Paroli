54b3209a374d9387da850aafb4dd4a27f7f554bd
MarcioPorto <mflporto@gmail.com>
2015-09-24 02:54:41 +0800

    added camera features

---
+++ app/src/main/java/com/marcioporto/android/ribbit/ParseConstants.java
@@ -0,0 +1,11 @@
+package com.marcioporto.android.ribbit;
+
+public final class ParseConstants {
+
+    // Class name
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+
+}
